16936c37d3d89d6ed58f969a46fbb1a1e1c6fa6d
msk007 <krawiec515@gmail.com>
2015-09-09 10:32:02 -0400

    made changes to test and game

--- stick_test.py
+++ stick_test.py
@@ -2,12 +2,16 @@ import unittest
 from sticks.py import *
 
 class TestSticks(unittest.TestCase):
-​
-​
-    def test_game_loop(self):
+​   def test_game_loop(self):
         # self.assertEqual(easy_words(word_list),
         # easy_words_list = []
-​
+        self.assertTrue(is_game_over(-5))​
+        self.assertTrue(is_game_over(-1))
+        self.assertTrue(is_game_over(0))
+        self.assertFalse(is_game_over(1))
+        self.assertFalse(is_game_over(2))
+        self.assertFalse(is_game_over(3))
+        self.assertFalse(is_game_over(50))
         pass
 ​
 ​
--- sticks.py
+++ sticks.py
@@ -1,25 +1,41 @@
 import random
 
-oponent=input("Would uou like to play against player 2 or the computer? Press 1 for computer or 2 for player 2: ")
-if input =='1':
-    oponent=computer
-if input =='2':
-    oponent=player
+def main():
+    print("welcome to game of sticks!!!!!!!")
+    stcks= sticknum()
 
-sticks=int(0)
+    opponent=input("Would uou like to play against player 2 or the computer? Press 1 for computer or 2 for player 2: ")
+        if input =='1':
+    opponent=computer
+        if input =='2':
+    opponent=player2
+
 sticks_left=int(0)
-def opponent():
+def sticknum():
     while opponent=='2' and opponent=='1':
-        sticknum=input("What is the number of sticky you want to start with (10-100) ")
-        sticks=int(sticknum)
+        sticks=input("What is the number of sticky you want to start with? (10-100) ")
         if sticks >=10 and sticks<=100:
             print ("there are {} sticks in the pile." ).format(sticks)
         if sticks<10 or sticks>100:
             print "Number of sticks must be between 10 and 100."
 
+opponent='1'
+    while True:#human vs ai
+        print ("there are {} sticks in the pile." ).format(sticks)
+    sticks -= sticks_out(cur_player)
+    if is_game_over(sticks):
+        break
+    if opponent==1:
+        opponent=2
+    else:
+        opponent=1
+print "You loose"
+
+
+
         break
 
-print "Sticks left: " , sticks
+print "Sticks left: " , sticknum
 sticks_taken =int(input("Take how many sticks(1-3):"))
 if sticks_taken >=3 or sticks_taken <1:
     print "Must choose 1-3"
@@ -38,9 +54,6 @@ def computer():
             print("Player 1 took {} sticks.").format(sticks)
             sticks-=player1
             print("There are {} sticks left in the pile.").format(sticks)
-
-        break
-
     computer==random.randint(1,3)
     print("computer took {} sticks").format(sticks_taken)
     sticks-=ai
