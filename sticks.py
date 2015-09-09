0989ffd624cfbedfcb6f068f7ccf9410582c54e4
msk007 <krawiec515@gmail.com>
2015-09-09 00:09:31 -0400

    made change
    changed if to while

--- sticks.py
+++ sticks.py
@@ -8,7 +8,7 @@ oponent=input("Would uou like to play against player 2 or the computer? Press 1 
 
 sticks=int(0)
 sticks_left=int(0)
-if opponent==2 and opponent==1
+while opponent=='2' and opponent=='1'
     sticknum=input(What is the number of sticky you want to start with (10-100) ")
     sticks=int(sticknum)
     if sticks >=10 and sticks<=100
@@ -19,9 +19,29 @@ if opponent==2 and opponent==1
         break
 
 print "Sticks left: " , sticks
-    sticks_taken = int(raw_input("Take how many sticks(1-3):"))
-    if sticks == 1:
-        print "You took the last stick, you loose"
+    sticks_taken =int(input("Take how many sticks(1-3):"))
     if sticks_taken >=3 or sticks_taken <1:
-        print "Must choose1-3"
-    
+        print "Must choose 1-3"
+        continue
+
+    if sticks == 1:
+        print "You loose"
+
+ai=computer
+computer=int(0)
+while opponent=='1' and sticks>=2:
+    playerstr==input("Player 1, enter the number of sticks to take: (1-3) -->")
+    player1=int(player1str)
+    if player>=1 and player<=3:
+        print("Player 1 took {} sticks.").format(sticks)
+        sticks-=player1
+        print("There are {} sticks left in the pile.").format(sticks)
+
+        break
+
+    elif print("You must pick a correct number of sticks to take. (1-3)")
+    computer==random.randint(1,3)
+    print("computer took {} sticks").format(sticks_taken)
+    sticks-=ai
+    if sticks==1:
+        print "you win!"
