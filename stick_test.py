15413c3ee5d4811f499ba09ba78c82aedc705dbd
msk007 <krawiec515@gmail.com>
2015-09-09 00:26:22 -0400

    last fix
    last try. got it to ask what player

--- stick_test.py
+++ stick_test.py
@@ -1,2 +1,15 @@
 import unittest
 from sticks.py import *
+
+class TestSticks(unittest.TestCase):
+​
+​
+    def test_game_loop(self):
+        # self.assertEqual(easy_words(word_list),
+        # easy_words_list = []
+​
+        pass
+​
+​
+if __name__ == '__main__':
+    unittest.main()
--- sticks.py
+++ sticks.py
@@ -1,17 +1,17 @@
 import random
 
 oponent=input("Would uou like to play against player 2 or the computer? Press 1 for computer or 2 for player 2")
-    if input=1:
-        oponent=computer
-    if input=2
-        oponent=player
+if input =='1':
+    oponent=computer
+if input =='2':
+    oponent=player
 
 sticks=int(0)
 sticks_left=int(0)
-while opponent=='2' and opponent=='1'
-    sticknum=input(What is the number of sticky you want to start with (10-100) ")
+while opponent=='2' and opponent=='1':
+    sticknum=input("What is the number of sticky you want to start with (10-100) ")
     sticks=int(sticknum)
-    if sticks >=10 and sticks<=100
+    if sticks >=10 and sticks<=100:
         print ("there are {} sticks in the pile." ).format(sticks)
     if sticks<10 or sticks>100:
         print "Number of sticks must be between 10 and 100."
@@ -19,10 +19,10 @@ while opponent=='2' and opponent=='1'
         break
 
 print "Sticks left: " , sticks
-    sticks_taken =int(input("Take how many sticks(1-3):"))
-    if sticks_taken >=3 or sticks_taken <1:
-        print "Must choose 1-3"
-        continue
+sticks_taken =int(input("Take how many sticks(1-3):"))
+if sticks_taken >=3 or sticks_taken <1:
+    print "Must choose 1-3"
+    
 
     if sticks == 1:
         print "You loose"
@@ -39,7 +39,6 @@ while opponent=='1' and sticks>=2:
 
         break
 
-    elif print("You must pick a correct number of sticks to take. (1-3)")
     computer==random.randint(1,3)
     print("computer took {} sticks").format(sticks_taken)
     sticks-=ai
