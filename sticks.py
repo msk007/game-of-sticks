fd275fd3aac9c8b52f74c05f5b96810357f3502b
msk007 <krawiec515@gmail.com>
2015-09-08 23:04:41 -0400

    setting up sticks

---
+++ sticks.py
@@ -0,0 +1,15 @@
+import random
+
+oponent=input("Would uou like to play against player 2 or the computer? Press 1 for computer or 2 for player 2")
+    if input=1:
+        oponent=computer
+    if input=2
+        oponent=player
+
+sticks=int(0)
+sticks_left=int(0)
+if opponent==2 and opponent==1
+    sticknum=input(What is the number of sticky you want to start with (10-100) ")
+    sticks=int(sticknum)
+    if sticks >=10 and sticks<=100
+        print ("there are {} sticks in the pile." ).format(sticks)
